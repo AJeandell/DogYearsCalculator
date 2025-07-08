0a3a7f103b1cabb2f8a9daeb87a2aa1ade480d54
Austin Jeandell <70723035+AJeandell@users.noreply.github.com>
2025-07-08 12:15:12 -0400

    Update DogYearsCalculator.cpp
    Made more universal

--- DogYearsCalculator.cpp
+++ DogYearsCalculator.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 
 int main() {
-  int dog_age = 1; //dogs age
+  int dog_age = 0; //dogs age
   int early_years = 21; // dogs age at 21 human years in first 2 years
   int later_years = (dog_age - 2) * 4; //dogs aging changes after 2 years to 4 human years equivalent
   int human_years;
+  std::string dog_name = "";
   human_years = early_years+later_years;
 
-  std::cout << "My name is Chocquito! Ruff ruff, I am " << human_years << " years old in human years.";
+  std::cout << "Enter dogs name";
+  std::cin >> dog_name;
+  std::cout << "Enter dogs age";
+  std::cin >> dog_age;
+  std::cout << "My name is " << dog_name << "! Ruff ruff, I am " << human_years << " years old in human years.";
 }
